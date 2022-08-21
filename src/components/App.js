d8bb2276bc90118f5c558e53e8774a8a199b7bde
Janice <janiceralecha@gmail.com>
2022-08-21 12:01:18 -0500

    commit tree 4b60ca14e106c7dd6df2f643f4826200d1650541

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
-
 function App() {
+const [listing, setListing] = useState([]);
+
+useEffect (() => {
+  fetch("http://localhost:6001/listings")
+  .then(response => response.json())
+  .then(data => setListing(data))
+}, [])
+
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listing} />
     </div>
   );
 }
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ListingCard() {
-  return (
-    <li className="card">
-      <div className="image">
-        <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
-      </div>
-      <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
-        ) : (
-          <button className="emoji-button favorite">☆</button>
-        )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
-      </div>
-    </li>
-  );
-}
+function ListingCard({listings}) {
+
+  const [favorite, setFavorite] = useState(false)
+  const [remove, setRemove] = useState(true)
+
+  function handleClick(){
+    setFavorite(!favorite)
+  }
+
+  function handleRemove(){
+    setRemove(!remove)
+    }
+
+    return (
+      <li className="card">
+        <div className="image">
+          <span className="price">$0</span>
+          <img src={listings.image} alt={listings.name} />
+        </div>
+        <div className="details">
+          {favorite ? (
+          <button  onClick={handleClick}  className="emoji-button favorite active">★</button>
+          ) : (
+            <button onClick={handleClick} className="emoji-button favorite">☆</button>
+          )}
+          <strong>{listings.description}</strong>
+          <span> · {listings.location}</span>
+          <button onClick={handleRemove}  className="emoji-button delete">🗑</button>
+        </div>
+      </li>
+    );
+  }
 
 export default ListingCard;
